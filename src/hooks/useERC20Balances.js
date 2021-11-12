b62a34c5b21cb9b4ba475134045d807926a7583d
BanalitoRaulito <raul.volmar@gmail.com>
2021-11-12 23:37:53 +0200

    FEATURE: Add sorting

--- src/hooks/useERC20Balances.js
+++ src/hooks/useERC20Balances.js
@@ -1,40 +1,41 @@
-import { useEffect, useState } from "react";
-import { useMoralis, useMoralisWeb3Api } from "react-moralis";
-import { useMoralisDapp } from "providers/MoralisDappProvider/MoralisDappProvider";
-import {networkConfigs} from "../helpers/networks";
+import {useEffect, useState} from "react";
+import {useMoralis, useMoralisWeb3Api} from "react-moralis";
+import {useMoralisDapp} from "providers/MoralisDappProvider/MoralisDappProvider";
+import {networkConfigs, disabledNetworks} from "../helpers/networks";
 
-export const useERC20Balances = (params) => {
-  const { account } = useMoralisWeb3Api();
-  const { isInitialized } = useMoralis();
-  const { walletAddress, chainId } = useMoralisDapp();
+export const useERC20Balances = () => {
+  const {account} = useMoralisWeb3Api();
+  const {isInitialized} = useMoralis();
+  const {walletAddress, chainId} = useMoralisDapp();
 
-  const [assets, setAssets] = useState();
+  const [assets, setAssets] = useState([]);
 
   useEffect(() => {
     if (isInitialized) {
       fetchERC20Balances()
-        .then((balance) => setAssets(balance))
         .catch((e) => alert(e.message));
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isInitialized, chainId, walletAddress]);
 
   const fetchERC20Balances = async () => {
-    let balances = []
-    let networks = Object.entries(networkConfigs)
-    let disabledNetworks = ['0x539']
+    let networks = Object.keys(networkConfigs);
 
-    networks.filter(network => !disabledNetworks.includes(network[0]))
-        .forEach(chain => {
-      balances.push(account.getTokenBalances({ address: walletAddress, chain: chain[0] }));
-    })
-
-    const assetBalance = await Promise.all(balances)
-      .then((result) => result)
-      .catch((e) => alert(e.message));
-
-    return [].concat.apply([], assetBalance)
+    networks
+      .filter(chain => !disabledNetworks.includes(chain))
+      .forEach(chainId => {
+        account
+          .getTokenBalances({address: walletAddress, chain: chainId})
+          .then(tokenBalances => tokenBalances.map(tokenBalance => ({
+            ...tokenBalance,
+            chainId,
+          })))
+          .then(tokenBalances => {
+            console.log(tokenBalances)
+            setAssets(prevAssets => [...prevAssets, ...tokenBalances]);
+          })
+      })
   };
 
-  return { fetchERC20Balances, assets };
+  return {fetchERC20Balances, assets};
 };
