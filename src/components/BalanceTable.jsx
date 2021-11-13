69808717ce6361c25129e92f5ed6a6d2007789cc
Roland Perli <roland.perli@paxful.com>
2021-11-13 12:53:21 +0200

    Refactor filters

---
+++ src/components/BalanceTable.jsx
@@ -0,0 +1,60 @@
+import {useMoralis} from "react-moralis";
+import {Table} from "antd";
+import {getEllipsisTxt} from "../helpers/formatters";
+
+const BalanceTable = ({assets}) => {
+  const {Moralis} = useMoralis();
+
+  const columns = [
+    {
+      title: "",
+      dataIndex: "logo",
+      key: "logo",
+      render: (logo) => (
+        <img
+          src={logo || "https://etherscan.io/images/main/empty-token.png"}
+          alt="nologo"
+          width="28px"
+          height="28px"
+        />
+      ),
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (name) => name,
+    },
+    {
+      title: "Symbol",
+      dataIndex: "symbol",
+      key: "symbol",
+      render: (symbol) => symbol,
+    },
+    {
+      title: "Balance",
+      dataIndex: "balance",
+      key: "balance",
+      render: (value, item) =>
+        parseFloat(Moralis.Units.FromWei(value, item.decimals).toFixed(6)),
+    },
+    {
+      title: "Address",
+      dataIndex: "token_address",
+      key: "token_address",
+      render: (address) => getEllipsisTxt(address, 5),
+    },
+  ];
+
+  return (
+    <Table
+      dataSource={assets}
+      columns={columns}
+      rowKey={(record) => {
+        return record.token_address;
+      }}
+    />
+  );
+}
+
+export default BalanceTable;
